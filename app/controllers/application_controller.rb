df0689d3e1be5457d13a2bfb98196106e7192624
keito0tada <keito0tada@gmail.com>
2024-04-01 14:51:12 +0900

    add uuid and email unique to User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
+$SERVICE_NAME = "チャットアプリ（仮）" # rubocop:disable Style/GlobalVars
+
 class ApplicationController < ActionController::Base
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,18 @@
 
 # User
 class User < ApplicationRecord
+  before_save :downcase_email
+
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 8 }
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
